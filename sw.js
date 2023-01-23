6093f9428c079816206d9952b00d5acef1b27655
JKinsella04 <>
2023-01-23 10:51:08 -0500

    Update documentation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230123.102706';
+const cacheName = 'chirpy-20230123.104912';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
